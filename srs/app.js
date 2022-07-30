b5b08009573040eb9fa965426c982c135a703bc8
VickyBondarenko <quikyv@gmail.com>
2022-07-30 21:28:06 +0200

    Add function celsius link

--- srs/app.js
+++ srs/app.js
@@ -45,8 +45,10 @@ function formatDate(date) {
   let year = dateNow.getFullYear();
   return `${dates}.${month}.${year}`;
 }
+
 function showTemperature(response) {
-  let temp = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+  let temp = Math.round(celsiusTemperature);
   let currentTemperature = document.querySelector("#temp");
   currentTemperature.innerHTML = temp;
 
@@ -65,7 +67,14 @@ function showTemperature(response) {
   let city = response.data.name;
   let currentCity = document.querySelector("#city");
   currentCity.innerHTML = city;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
+
 function search(city) {
   let apiKey = "f127cb208f2bd0106804f1fe6bc22525";
   let units = "metric";
@@ -81,13 +90,6 @@ function handleSubmit(event) {
   let city = curentCityElement.value;
   search(city);
 }
-let currentTime = new Date();
-let dayTimeElement = document.querySelector("#dayTime");
-dayTimeElement.innerHTML = formatDayTime(currentTime);
-let dateElement = document.querySelector("#date");
-dateElement.innerHTML = formatDate(currentTime);
-let searchForm = document.querySelector("#enter-city");
-searchForm.addEventListener("submit", handleSubmit);
 
 function showPosition(position) {
   console.log(position.coords.latitude);
@@ -98,10 +100,44 @@ function showPosition(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(showTemperature);
 }
+
 function getCurrentPosition(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
 }
+
+function changeFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+}
+
+function changeCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", changeFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", changeCelsius);
+
 let button = document.querySelector("button");
 button.addEventListener("click", getCurrentPosition);
+let currentTime = new Date();
+let dayTimeElement = document.querySelector("#dayTime");
+dayTimeElement.innerHTML = formatDayTime(currentTime);
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = formatDate(currentTime);
+let searchForm = document.querySelector("#enter-city");
+searchForm.addEventListener("submit", handleSubmit);
 search("Dnipro");
